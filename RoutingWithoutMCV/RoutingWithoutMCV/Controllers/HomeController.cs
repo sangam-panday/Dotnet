ede74e92c73a83190fdcd7a27eb04c9c831a9267
sangamxp <160036687+sangamxp@users.noreply.github.com>
2025-03-29 09:48:10 +0545

    Routing Without MCV

---
+++ RoutingWithoutMCV/RoutingWithoutMCV/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingWithoutMCV.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route("~/")]
+        [Route("~/Home")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        [Route("{id?}")]
+        public int Details(int? id)
+        {
+            return id ?? 1;
+        }
+    }
+}
